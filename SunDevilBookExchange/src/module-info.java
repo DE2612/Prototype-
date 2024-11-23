7482bb56cc778a33fc4a240c5e14acfb69154ec9
LamaPoitsky <111145557+DE2612@users.noreply.github.com>
2024-11-23 09:03:37 -0700

    Delete SunDevilBookExchange directory

--- SunDevilBookExchange/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module SunDevilBookExchange {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires java.desktop;
-	requires javafx.swing;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
